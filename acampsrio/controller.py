65cbc1e86568766c40675ad3034387c79e9e9c8d
Matheus Cardoso <mtzimba@gmail.com>
2016-10-27 21:55:10 -0200

    Retirado Indicado por do csv

--- acampsrio/controller.py
+++ acampsrio/controller.py
@@ -348,7 +348,6 @@ class ExportarParticipantesHandler(RequestHandler):
                              "Problema de Saude", "Restricao Atividade Fisica", "Tem Alguma Alergia", "Toma Algum Medicamento",
                              "Nome do Contato",
                              "Tel Celular1 Contato", "Tel Celular2 Contato", "Tel Residencial Contato", "Tel Comercial Contato",
-                             "Indicado por",
                              "Familia", "Pagou a Inscricao", "Ja Chegou"])
             
             for participante in Participante.all().order('nome'):
@@ -374,7 +373,6 @@ class ExportarParticipantesHandler(RequestHandler):
                                  smart_str(participante.telCelular2Contato, encoding='ISO-8859-1'),
                                  smart_str(participante.telResidencialContato, encoding='ISO-8859-1'),
                                  smart_str(participante.telComercialContato, encoding='ISO-8859-1'),
-                                 smart_str(participante.indicadoPor, encoding='ISO-8859-1'),
                                  smart_str(participante.familia, encoding='ISO-8859-1'),
                                  smart_str(participante.pagouInscricao, encoding='ISO-8859-1'),
                                  smart_str(participante.jaChegou, encoding='ISO-8859-1')
